35b7f8a1883917f8fe1dfe7fa5bed79a97f6c8e9
Patrick Sanchez <mpgow22@gmail.com>
2025-04-20 17:23:34 -0700

    added .gitignore for .vs/ local config file. Also added tamagotchi as a loaded 3d model, although it has no lighting and is clipped inside the rotating cube still.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+const loader = new GLTFLoader();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -12,6 +14,17 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+loader.load('public/tamagotchi.gltf', function (gltf) {
+
+    scene.add(gltf.scene);
+
+}, undefined, function (error) {
+
+    console.error(error);
+
+});
+
 
 camera.position.z = 5;
 
